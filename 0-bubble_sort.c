e9a5267d0d241974b3bdcc641df54f933643a230
Marwane <khmarwane10@gmail.com>
2023-11-17 09:09:12 -0800

    Sorting algorithms & Big O

---
+++ 0-bubble_sort.c
@@ -0,0 +1,36 @@
+#include "sort.h"
+
+/**
+ * bubble_sort - Sorts an array of integers in ascending order using
+ *	Bubble Sort.
+ * @array: The array to be sorted.
+ * @size: The number of elements in the array.
+ */
+
+void bubble_sort(int *array, size_t size)
+{
+	size_t i, j;
+	int tmp;
+
+	/* Check if the array is NULL or if it has less than 2 elements */
+	if (array == NULL || size < 2)
+		return;
+
+	for (i = 0; i < size - 1; i++)
+	{
+		for (j = 0; j < size - i - 1; j++)
+		{
+			/* comparison of two consecutive elements */
+			if (array[j] > array[j + 1])
+			{
+				/* Store and swap array[j] and array[j + 1]*/
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+
+				/* Print the array after each swap */
+				print_array(array, size);
+			}
+		}
+	}
+}
---
+++ README.md
@@ -0,0 +1,11 @@
+C - Sorting algorithms & Big O
+
+Tasks: mandatory
+
+0. Bubble sort
+
+1. Insertion sort
+
+2. Selection sort
+
+3. Quick sort
